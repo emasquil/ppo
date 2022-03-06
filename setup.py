bd2ade92139a270e733aaa24ce1b7778c6b32d2b
Théo <theo.vincent@eleves.enpc.fr>
2022-03-06 13:09:31 +0100

    Change src to ppo and requirements to .cfg

--- setup.py
+++ setup.py
@@ -1,14 +1,4 @@
 from setuptools import setup
 
 
-setup(
-    name="ppo",
-    version="0.1",
-    description="Simple PPO implementation on OpenAI GYM environment.",
-    packages=["src"],
-    requires=["setuptools", "wheel"],
-    install_requires=["dm-env==1.5", "gym==0.22.0", "pygame==2.1.2"],
-    extras_require={
-        "dev": ["tqdm==4.63.0", "ipykernel==6.9.1", "black==22.1.0"],
-    },
-)
+setup()
