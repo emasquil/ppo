fa6dd27a53634abc6fe7182769648c2511eeea35
Théo <theo.vincent@eleves.enpc.fr>
2022-03-10 15:58:20 +0100

    Online ppo compiling

--- ppo/agents/online_ppo.py
+++ ppo/agents/online_ppo.py
@@ -16,21 +16,21 @@ from typing import Tuple
 class OnlinePPO(Actor):
     def __init__(
         self,
-        environment_spec: specs.EnvironmentSpec,
+        observation_spec: specs.BoundedArray,
         policy_network,
         value_network,
-        rng: chex.PRNGKey,
+        key: chex.PRNGKey,
         learning_rate: float = 1e-3,
         discount: float = 0.99,
     ):
-        policy_rng, value_rng = jax.split(rng, 2)
+        policy_key, value_key = jax.random.split(key, 2)
         self.policy_network = hk.without_apply_rng(hk.transform(policy_network))
         self.policy_params = self.policy_network.init(
-            rng=policy_rng, inputs=jnp.zeros(environment_spec.observations.shape)
+            rng=policy_key, observations=jnp.zeros(observation_spec.shape)
         )
         self.value_network = hk.without_apply_rng(hk.transform(value_network))
         self.value_params = self.policy_network.init(
-            rng=value_rng, inputs=jnp.zeros(environment_spec.observations.shape)
+            rng=value_key, observations=jnp.zeros(observation_spec.shape)
         )
 
         self.policy_optimizer = optax.adam(learning_rate)
@@ -38,15 +38,15 @@ class OnlinePPO(Actor):
         self.value_optimizer = optax.adam(learning_rate)
         self.value_optimizer_state = self.value_optimizer.init(self.value_params)
 
-        def greedy_policy(policy_params, rng, observation):
+        def greedy_policy(policy_params: hk.Params, key: chex.PRNGKey, observation: np.ndarray):
             mu, sigma = self.policy_network.apply(policy_params, observation)
 
-            return rlax.gaussian_diagonal().sample(rng, mu, sigma)
+            return rlax.gaussian_diagonal().sample(key, mu, sigma)
 
         self.greedy_policy = greedy_policy
-        self.greedy_rng = hk.PRNGSequence(1)
+        self.greedy_keys = hk.PRNGSequence(1)
 
-        def policy(policy_params, observation):
+        def policy(policy_params: hk.Params, observation: np.ndarray):
             return self.policy_network.apply(policy_params, observation)[0]
 
         self.policy = policy
@@ -60,7 +60,7 @@ class OnlinePPO(Actor):
         # Convert to get a batch shape
         observation = tree_util.tree_map(lambda x: jnp.expand_dims(x, axis=0), observation)
 
-        action = self.greedy_policy(self.policy_params, next(self.greedy_rng), observation)
+        action = self.greedy_policy(self.policy_params, next(self.greedy_keys), observation)
 
         # Convert back to single action
         action = tree_util.tree_map(lambda x: jnp.array(x).squeeze(axis=0), action)
