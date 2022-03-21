949c3a3e08e27591f941bd8a2ebc49fa31ea54d3
Théo <theo.vincent@eleves.enpc.fr>
2022-03-21 18:13:13 +0100

    Policy loss

--- ppo/agents/vanilla_ppo.py
+++ ppo/agents/vanilla_ppo.py
@@ -2,8 +2,13 @@ import numpy as np
 from dataclasses import replace
 
 import dm_env
+import jax
 import jax.numpy as jnp
+from jax.experimental.optimizers import clip_grads
+import rlax
 import chex
+import optax
+import haiku as hk
 from acme import specs
 
 from ppo.agents.base_agent import BaseAgent
@@ -26,9 +31,13 @@ class VanillaPPO(BaseAgent):
         key: chex.PRNGKey,
         learning_rate: float = 1e-3,
         discount: float = 0.99,
+        clipping_ratio_threshold: float=0.1,
+        max_grad_norm: float=0.5,
     ):
         super(VanillaPPO, self).__init__(observation_spec, policy_network, value_network, key, learning_rate, discount)
         self.replay_buffer = FixedReplayBuffer(buffer_capacity=50)
+        self.clipping_ratio_threshold = clipping_ratio_threshold
+        self.max_grad_norm = max_grad_norm
 
     def observe_first(self, timestep: dm_env.TimeStep) -> None:
         self.replay_buffer.add_first(timestep)
@@ -42,58 +51,37 @@ class VanillaPPO(BaseAgent):
     def value_loss(self, value_params, batch):
         pass
 
-    def policy_loss(self, policy_params, batch):
-        # compute ratio
+    def policy_loss(self, policy_params: hk.Params, batch: Transition):
+        replace(batch, advantage_t=(batch.advantage_t - jnp.mean(batch.advantage_t, axis=0)) / (jnp.std(batch.advantage_t, axis=0) + 1e-8))
+       
+        mu, sigma = self.policy_network.apply(policy_params, batch.observation_t)
+        log_probability_t = rlax.gaussian_diagonal().logprob(batch.action_t, mu, sigma)
+        log_ratio = log_probability_t - batch.log_probability_t
+        ratio = jnp.exp(log_ratio)
+        
+        clipped_loss = jnp.minimum(ratio * batch.advantage_t, jax.lax.clamp(1 - self.clipping_ratio_threshold, ratio, 1 - self.clipping_ratio_threshold) * batch.advantage_t)
+        
+        return - jnp.mean(clipped_loss)
 
-        #
-        pass
+    def update(self, batch: Transition):
+        # Update value network
+        # (_, advantage), value_gradients = jax.value_and_grad(
+        #     self.value_loss, has_aux=True
+        # )(self.value_params, self.timestep, self.next_timestep)
+        # advantage = jax.lax.stop_gradient(advantage)
 
-    def update_on_batch(self, batch: Transition):
-        # Normalize batch advantage
-        batch.advantage_t
-        replace(batch, advantage_t=batch.advantages_t - jnp.mean(batch.advantages_t, axis=0))
-        end = start + args.minibatch_size
-        mb_inds = b_inds[start:end]
+        # value_updates, self.value_optimizer_state = self.value_optimizer.update(
+        #     value_gradients, self.value_optimizer_state
+        # )
+        # self.value_params = optax.apply_updates(self.value_params, value_updates)
 
+        # Update policy network
+        policy_gradients = jax.grad(self.policy_loss, has_aux=True)(
+            self.policy_params, batch, 
+        )
+        clip_grads(policy_gradients, self.max_grad_norm)
 
-        _, newlogprob, entropy, newvalue = self.get_action_and_value(b_obs[mb_inds], b_actions.long()[mb_inds])
-        logratio = newlogprob - b_logprobs[mb_inds]
-        ratio = logratio.exp()
-
-        with torch.no_grad():
-            # calculate approx_kl http://joschu.net/blog/kl-approx.html
-            old_approx_kl = (-logratio).mean()
-            approx_kl = ((ratio - 1) - logratio).mean()
-            clipfracs += [((ratio - 1.0).abs() > args.clip_coef).float().mean().item()]
-
-        mb_advantages = b_advantages[mb_inds]
-        if args.norm_adv:
-            mb_advantages = (mb_advantages - mb_advantages.mean()) / (mb_advantages.std() + 1e-8)
-
-        # Policy loss
-        pg_loss1 = -mb_advantages * ratio
-        pg_loss2 = -mb_advantages * torch.clamp(ratio, 1 - args.clip_coef, 1 + args.clip_coef)
-        pg_loss = torch.max(pg_loss1, pg_loss2).mean()
-
-        # Value loss
-        newvalue = newvalue.view(-1)
-        if args.clip_vloss:
-            v_loss_unclipped = (newvalue - b_returns[mb_inds]) ** 2
-            v_clipped = b_values[mb_inds] + torch.clamp(
-                newvalue - b_values[mb_inds],
-                -args.clip_coef,
-                args.clip_coef,
-            )
-            v_loss_clipped = (v_clipped - b_returns[mb_inds]) ** 2
-            v_loss_max = torch.max(v_loss_unclipped, v_loss_clipped)
-            v_loss = 0.5 * v_loss_max.mean()
-        else:
-            v_loss = 0.5 * ((newvalue - b_returns[mb_inds]) ** 2).mean()
-
-        entropy_loss = entropy.mean()
-        loss = pg_loss - args.ent_coef * entropy_loss + v_loss * args.vf_coef
-
-        optimizer.zero_grad()
-        loss.backward()
-        nn.utils.clip_grad_norm_(agent.parameters(), args.max_grad_norm)
-        optimizer.step()
+        policy_updates, self.policy_optimizer_state = self.policy_optimizer.update(
+            policy_gradients, self.policy_optimizer_state
+        )
+        self.policy_params = optax.apply_updates(self.policy_params, policy_updates)
