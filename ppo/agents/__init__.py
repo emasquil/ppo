d257c8600dc1627055beb21277445371b4377253
paulinesert <sertpauline@gmail.com>
2022-03-27 18:22:27 +0000

    default hyperparams + general_estimation typo + update notebook

--- ppo/agents/__init__.py
+++ ppo/agents/__init__.py
@@ -1,5 +1,5 @@
 from .base_agent import BaseAgent
 from .random_agent import RandomAgent
-from .advantage import general_advantage_estmation
+from .advantage import general_advantage_estimation
 from .online_ppo import OnlinePPO
 from .vanilla_ppo import VanillaPPO
