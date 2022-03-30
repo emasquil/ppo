53d790dd724f25ce387df0f5bba847537df2a0e5
nviolante25 <nviolante96@gmail.com>
2022-03-30 20:05:57 +0200

    move test to separete file

--- ppo/replay_buffers/fixed_replay_buffer.py
+++ ppo/replay_buffers/fixed_replay_buffer.py
@@ -40,39 +40,3 @@ class FixedReplayBuffer(BaseReplayBuffer):
 
         # we add the transition to the current episode list of transitions
         self._memory[-1].append(transition)
-
-
-if __name__ == "__main__":
-    from ppo.env_wrapper import PendulumEnv
-
-    replay_buffer = FixedReplayBuffer(jax.random.PRNGKey(0))
-
-    env = PendulumEnv()
-
-    num_episodes = 3
-    for e in range(num_episodes):
-        timestep = env.reset()
-        replay_buffer.add_first(timestep)
-
-        rewards = []
-        observations = []
-        num_steps = 5
-        for t in range(num_steps):
-            action = 0.5 * np.ones((1,))
-            observations.append(timestep.observation)
-            timestep = env.step(action)
-            replay_buffer.add(t, t, action, timestep)
-
-            rewards.append(timestep.reward)
-            if t == num_steps:
-                observations.append(timestep.observation)
-
-        observations = np.array(observations)
-        rewards = np.array(rewards)
-        replay_observatons = np.array([np.array(transition.observation_t) for transition in replay_buffer._memory[-1]])
-        replay_rewards = np.array([np.array(transition.reward_tp1) for transition in replay_buffer._memory[-1]])
-
-        assert np.all(np.isclose(replay_observatons, observations))
-        assert np.all(np.isclose(replay_rewards, rewards))
-
-        replay_buffer._memory.append([])
